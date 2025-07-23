14f5d58c1dd34ecf9969399ea91ad90b3d1327d6
Krystian Bajno <krystian.bajno@gmail.com>
2025-07-23 22:56:46 +0200

    Fixed base64

--- src/lib/encoder/encodeBase64NoPadding.js
+++ src/lib/encoder/encodeBase64NoPadding.js
@@ -1,3 +1,5 @@
+import { encodeBase64 } from "./encodeBase64";
+
 export function encodeBase64NoPadding(s) {
   return encodeBase64(s).replace(/=/g, '');
 }
--- src/lib/encoder/encodeBase64Safe.js
+++ src/lib/encoder/encodeBase64Safe.js
@@ -1,3 +1,5 @@
+import { encodeBase64 } from "./encodeBase64";
+
 export function encodeBase64Safe(s) {
   // Base64 with URL and filename safe alphabet
   return encodeBase64(s).replace(/\+/g, '-').replace(/\//g, '_');
